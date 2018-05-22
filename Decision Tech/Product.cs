d55cbcf026ff61dc9b0973b7d260348c89b5d2c4
Przemyslaw Gara <przemyslaw.gara@hotmail.com>
2018-05-22 22:11:37 +0100

    Some changes

---
+++ Decision Tech/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Decision_Tech
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public decimal ProductCost { get; set; }
+    }
+}
---
+++ Decision Tech/Products.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decision_Tech
+{
+    public class Products
+    {
+        public Product Butter { get; set; }
+        public Product Milk { get; set; }
+        public Product Bread { get; set; }
+    }
+}
